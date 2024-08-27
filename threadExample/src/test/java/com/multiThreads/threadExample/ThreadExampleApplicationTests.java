8db35552f470713008571cc0e0e1a7588591b35e
salih72 <salihcesur72@gmail.com>
2024-08-27 16:42:31 +0300

    projectCreated

---
+++ threadExample/src/test/java/com/multiThreads/threadExample/ThreadExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.multiThreads.threadExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThreadExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
